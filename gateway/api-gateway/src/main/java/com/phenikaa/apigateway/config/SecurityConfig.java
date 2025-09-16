642deceae3eb9c490e00e1a1366035ab32c0ecb4
HieuDoanThe <Thehieu0814@gmail.com>
2025-09-17 00:47:34 +0700

    cors config

--- gateway/api-gateway/src/main/java/com/phenikaa/apigateway/config/SecurityConfig.java
+++ gateway/api-gateway/src/main/java/com/phenikaa/apigateway/config/SecurityConfig.java
@@ -92,6 +92,7 @@ public class SecurityConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://phenikaa-thesis-management-fe.vercel.app/");
         config.addAllowedOrigin("https://websocketking.com");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
