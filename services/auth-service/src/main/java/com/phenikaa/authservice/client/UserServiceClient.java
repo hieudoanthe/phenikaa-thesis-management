ff7ba5739a954932574a7733aa2c999b2e4024df
HieuDoanThe <Thehieu0814@gmail.com>
2025-09-17 12:52:08 +0700

    eureka user-service change

--- services/auth-service/src/main/java/com/phenikaa/authservice/client/UserServiceClient.java
+++ services/auth-service/src/main/java/com/phenikaa/authservice/client/UserServiceClient.java
@@ -3,6 +3,7 @@ package com.phenikaa.authservice.client;
 import com.phenikaa.dto.request.LoginRequest;
 import com.phenikaa.dto.request.SaveRefreshTokenRequest;
 import com.phenikaa.dto.response.AuthenticatedUserResponse;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,23 +15,27 @@ public class UserServiceClient {
 
     private final WebClient.Builder webClientBuilder;
 
-    private final String userServiceUrl = "http://localhost:8081";
+    private final String userServiceUrl =
+            System.getenv().getOrDefault("USER_SERVICE_URL", "http://localhost:8081");
+
+    private WebClient webClient;
+
+    @PostConstruct
+    public void init() {
+        this.webClient = webClientBuilder.baseUrl(userServiceUrl).build();
+    }
+
 
     public Mono<AuthenticatedUserResponse> verifyUser(LoginRequest request) {
-        return webClientBuilder.baseUrl(userServiceUrl)
-                .build()
-                .post()
+        return webClient.post()
                 .uri("/internal/users/verify")
                 .bodyValue(request)
                 .retrieve()
                 .bodyToMono(AuthenticatedUserResponse.class);
     }
 
-
     public Mono<Void> saveRefreshToken(SaveRefreshTokenRequest request) {
-        return webClientBuilder.baseUrl(userServiceUrl)
-                .build()
-                .post()
+        return webClient.post()
                 .uri("/internal/users/save-refresh-token")
                 .bodyValue(request)
                 .retrieve()
@@ -39,9 +44,7 @@ public class UserServiceClient {
     }
 
     public Mono<Void> deleteRefreshToken(String refreshToken) {
-        return webClientBuilder.baseUrl(userServiceUrl)
-                .build()
-                .delete()
+        return webClient.delete()
                 .uri(uriBuilder -> uriBuilder
                         .path("/internal/users/delete-refresh-token")
                         .queryParam("token", refreshToken)
@@ -52,9 +55,7 @@ public class UserServiceClient {
     }
 
     public Mono<AuthenticatedUserResponse> getUserByRefreshToken(String refreshToken) {
-        return webClientBuilder.baseUrl(userServiceUrl)
-                .build()
-                .get()
+        return webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/internal/users/get-userBy-refresh-token")
                         .queryParam("token", refreshToken)
